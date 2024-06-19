3a17c6d62f9d9fd75765eb2fd81d426b47ec8fbd
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-06-19 17:54:29 -0300

    questionario

--- projeto_flask/app.py
+++ projeto_flask/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 lista_produtos = [
-    {"nome": "Coca-cola", "descricao": "veneno"},
-    {"nome": "Doritos", "descricao": "suja a mão"},
-    {"nome": "Agua", "descricao": "mata sede"},
+    {"nome": "Coca-cola", "descricao": "veneno", "preço": 8.99, "url": "https://gkpb.com.br/wp-content/uploads/2024/02/coca-cola-k-wave-gkpb-banner.jpg"},
+    {"nome": "Doritos", "descricao": "suja a mão", "preço": 15.99, "url": "https://wdsgroup.co.uk/wp-content/uploads/2022/04/08907.jpg" },
+    {"nome": "Agua", "descricao": "mata sede", "preço": 1.99, "url": "https://www.fusati.com.br/wp-content/uploads/2021/05/O-Que-e-agua-Mineral.jpeg" },
 ]
 
 app = Flask(__name__)
@@ -28,5 +28,20 @@ def produto(nome):
     
     return "Produto não existe!"
 
+
+# GET
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro_produto.html")
+
+# POST
+@app.route("/produtos", methods=["POST"])
+def salvar_produto():
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    produto = { "nome": nome, "descricao": descricao,}
+    lista_produtos.append(produto)
+    
+    return redirect(url_for("produtos"))
 
 app.run(port=5000)
